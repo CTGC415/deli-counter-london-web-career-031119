05a0165e8803e287f5e887df81a7cd76b505b2f4
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-03 20:25:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(line)
   if line.empty?
     puts "The line is currently empty."
   end
-  meassge = "The line is currently:"
-  line.each { |name| pu }
+  message = "The line is currently:"
+  line.each { |name| message += " #{line.index(name) + 1 }. #{name}" }
+  message
 end
